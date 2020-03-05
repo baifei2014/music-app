adf7fdc8e1ea9e5ac0bf7dc05d63179c0362e7c0
蒋龙豪 <jianglonghao@uniqueway.com>
2020-03-05 22:44:46 +0800

    聊天详情页

--- lib/pages/chat/chat_detail.dart
+++ lib/pages/chat/chat_detail.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:music/component/global/color.dart';
-import 'package:scoped_model/scoped_model.dart';
+import 'package:music/part/part.dart';
+import 'package:music/repository/cached_image.dart';
+import 'package:music/repository/netease.dart';
 
-import 'chat_input.dart';
 
 class ChatDetail extends StatelessWidget {
   final int targetUserId = 100;
@@ -12,24 +13,41 @@ class ChatDetail extends StatelessWidget {
       backgroundColor: LongColor.chat_app_bar,
       resizeToAvoidBottomInset: false,
       body: _BoxWithBottomChatController(
-        _ChatDetailTitle(targetUserId: 1232)
+        Loader(
+          loadTask: () => neteaseRepository.playlistDetail(115392005),
+          builder: (context, result) {
+            return _MainChatDetailPage();
+          },
+        ),
       )
     );
   }
 }
 
-class _ChatDetailTitle extends StatelessWidget {
-  final int targetUserId;
-  const _ChatDetailTitle({Key key, @required this.targetUserId}) : super(key: key);
+class _MainChatDetailPage extends StatefulWidget {
+  @override
+  _MainChatDetailPageState createState() {
+     return new _MainChatDetailPageState();
+  }
+}
+
+class _MainChatDetailPageState extends State {
+  List msgList;
 
   @override
+  void initState() {
+    super.initState();
+    msgList = new List();
+    for (var i = 0; i < 8; i+2) {
+      msgList.add({"name": "还欧艾斯", "id": i, "avatarUrl": "https://oss.likecho.com/user_avatar/109951164462932601.jpg"});
+      msgList.add({"name": "陈一发", "id": i+1, "avatarUrl": "http://p1.music.126.net/AlmamjLHkrppEmpP37N74g==/109951164770785633.jpg"});
+    }
+  }
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      top: true,
-      bottom: false,
-      child: AppBar(
+    return Scaffold(
+      appBar: AppBar(
         elevation: 0,
-        primary: false,
         leading: IconButton(
             tooltip: '返回上一层',
             icon: Icon(
@@ -38,19 +56,128 @@ class _ChatDetailTitle extends StatelessWidget {
             ),
             onPressed: () => Navigator.pop(context)),
         titleSpacing: 0,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              '陈一发',
-              style: TextStyle(fontSize: 18, color: LongColor.chat_font_theme),
-            ),
-          ]
+        title: Text(
+          '陈一发',
+          style: TextStyle(fontSize: 18, color: LongColor.chat_font_theme),
         ),
         backgroundColor: LongColor.chat_app_bar,
       ),
+      body: CustomScrollView(
+          slivers: <Widget>[
+            SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) => ChatMsgTile(msgList[index]),
+                  childCount: msgList.length),
+            ),
+          ]
+        ),
     );
+  }
+}
+
+class ChatMsgTile extends StatelessWidget {
+  final Map msg;
+
+  ChatMsgTile(this.msg, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      child: InkWell(
+        onTap: () {
+          // if (list.onMusicTap != null) list.onMusicTap(context, music);
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // (list.leadingBuilder ?? _buildPadding)(context, music),
+            Expanded(
+              child: _SimpleMsgTile(msg),
+            ),
+            // (list.trailingBuilder ?? _buildPadding)(context, music),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SimpleMsgTile extends StatelessWidget {
+  final Map msg;
+
+  const _SimpleMsgTile(this.msg, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print(msg);
+    Widget cover = Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+        child: FadeInImage(
+          placeholder: AssetImage("assets/playlist_playlist.9.png"),
+          image: CachedImage(msg['avatarUrl']),
+          fit: BoxFit.cover,
+          height: 40,
+          width: 40,
+        ),
+      ),
+    );
+    return Container(
+        height: 60,
+        child: Row(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(left: 16)),
+            cover,
+            Padding(padding: EdgeInsets.only(left: 16)),
+            Expanded(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(color: Colors.black12, width: 1))///边框颜色、宽
+                ),
+                child: Row(children: <Widget>[
+                  Text(
+                    msg['id'] + msg['name'],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],)
+              ),
+            ),
+          ]
+        )
+    );
+    // return Container(
+    //   height: 56,
+    //   child: Row(
+    //     children: <Widget>[
+    //       Expanded(
+    //           child: Column(
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //             children: <Widget>[
+    //               Spacer(),
+    //               Text(
+    //                 msg['id'].toString(),
+    //                 maxLines: 1,
+    //                 overflow: TextOverflow.ellipsis,
+    //                 style: Theme.of(context).textTheme.body1,
+    //               ),
+    //               Padding(padding: EdgeInsets.only(top: 3)),
+    //               Text(
+    //                 msg['name'],
+    //                 maxLines: 1,
+    //                 overflow: TextOverflow.ellipsis,
+    //                 style: Theme.of(context).textTheme.caption,
+    //               ),
+    //               Spacer(),
+    //             ],
+    //           )
+    //         ),
+    //     ],
+    //   ),
+    // );
   }
 }
 
@@ -65,79 +192,62 @@ class _BoxWithBottomChatController extends StatelessWidget {
     final media = MediaQuery.of(context);
     //hide bottom player controller when view inserts
     //bottom too height (such as typing with soft keyboard)
+    print('box高度 :\n');
+    print(media.viewInsets.bottom);
     return Column(
       children: <Widget>[
         Expanded(child: child),
-        _BottomChatControllerBar(),
+        BottomChatBox(),
+        
         SizedBox(height: media.viewInsets.bottom)
       ],
     );
   }
 }
 
-///底部当前音乐播放控制栏
+
+class BottomChatBox extends StatefulWidget {
+  // final Animation<double> animation;
+
+  const BottomChatBox({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _BottomChatBoxState();
+  }
+}
+
 // class _BottomChatControllerBar extends StatelessWidget {
-//   const _BottomChatControllerBar({Key key}) : super(key: key);
+class _BottomChatBoxState extends State {
+  final TextEditingController _inputTextController = TextEditingController();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return InkWell(
-//       child: Card(
-//         margin: const EdgeInsets.all(0),
-//         shape: const RoundedRectangleBorder(
-//             borderRadius:
-//                 const BorderRadius.only(topLeft: const Radius.circular(4.0), topRight: const Radius.circular(4.0))),
-//         child: Container(
-//           height: 56,
-//           child: Row(
-//             children: <Widget>[
-//               Hero(
-//                 tag: "album_cover",
-//                 child: Container(
-//                   padding: const EdgeInsets.all(8),
-//                   child: AspectRatio(
-//                     aspectRatio: 1,
-//                     child: ClipRRect(
-//                       borderRadius: BorderRadius.all(Radius.circular(3)),
-//                       child: Container(color: Colors.grey)
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               Expanded(
-//                 child: DefaultTextStyle(
-//                   style: TextStyle(),
-//                   maxLines: 1,
-//                   overflow: TextOverflow.ellipsis,
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: <Widget>[
-//                       Spacer(),
-//                       Text(
-//                         '你好',
-//                         style: Theme.of(context).textTheme.body1,
-//                       ),
-//                       Padding(padding: const EdgeInsets.only(top: 2)),
-//                       Spacer(),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//               // _PauseButton(),
-//               IconButton(
-//                   tooltip: "当前播放列表",
-//                   icon: Icon(Icons.menu),
-//                   onPressed: () {
-//                   }),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  final FocusNode _focusNode = FocusNode();
 
-class _BottomChatControllerBar extends StatelessWidget {
+  String get inputMsg => _inputTextController.text;
+
+  set inputMsg(String value) {
+    _inputTextController.text = value;
+  }
+
+  void _updateInputMsg(String msg) {
+    if (msg.isEmpty) {
+      return;
+    }
+    print('输入消息 :\n');
+    print(msg);
+    // _focusNode.unfocus();
+    setState(() {
+      this.inputMsg = msg;
+    });
+  }
+
+  void flushInputMsg() {
+    // _focusNode.unfocus();
+    setState(() {
+      this.inputMsg = '';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
@@ -148,210 +258,54 @@ class _BottomChatControllerBar extends StatelessWidget {
       ),
       child: Row(
         children: [
-          Padding(padding: EdgeInsets.only(left: 16)),
+          Padding(padding: EdgeInsets.only(left: 12)),
           Expanded(
             child: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(4)),
               child: Container(
                 height: 45,
                 alignment: Alignment.centerLeft,
-                // color: Colors.blue[600],
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  // border: Border.all(color: Colors.black12, width: 1)///边框颜色、宽
                 ),
                 child: TextField(
-                  // controller: _queryTextController,
-                  // focusNode: _focusNode,
-                  // style: theme.primaryTextTheme.title,
+                  controller: _inputTextController,
+                  focusNode: _focusNode,
                   textInputAction: TextInputAction.search,
                   style: TextStyle(fontSize: 18),
                   // onSubmitted: (String _) => _search(query),
+                  onChanged: (String _) => _updateInputMsg(inputMsg),
                   decoration: InputDecoration(
                     fillColor: Colors.white,
                     border: InputBorder.none,
                     hintStyle: theme.primaryTextTheme.title,
-                    hintText: MaterialLocalizations.of(context).searchFieldLabel
+                    hintText: MaterialLocalizations.of(context).searchFieldLabel,
+                    contentPadding: EdgeInsets.fromLTRB(8, 0, 8, 0)
                   )
                 ),
               )
             ),
-            // child: Container(
-            //   height: 45,
-            //   alignment: Alignment.centerLeft,
-            //   // color: Colors.blue[600],
-            //   decoration: BoxDecoration(
-            //     color: Colors.white,
-            //     // border: Border.all(color: Colors.black12, width: 1)///边框颜色、宽
-            //   ),
-            //   child: TextField(
-            //     // controller: _queryTextController,
-            //     // focusNode: _focusNode,
-            //     // style: theme.primaryTextTheme.title,
-            //     textInputAction: TextInputAction.search,
-            //     // onSubmitted: (String _) => _search(query),
-            //     decoration: InputDecoration(
-            //       fillColor: Colors.white,
-            //       border: InputBorder.none,
-            //       hintStyle: theme.primaryTextTheme.title,
-            //       hintText: MaterialLocalizations.of(context).searchFieldLabel
-            //     )
-            //   ),
-            // )
           ),
-          Padding(padding: EdgeInsets.only(left: 16)),
+          SizedBox(width: 16,),
+          ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            child: Container(
+              height: 45,
+              alignment: Alignment.center,
+              child: RaisedButton(
+                child: Text('发送', style: TextStyle(fontSize: 18),),
+                onPressed: () {
+                  flushInputMsg();
+                },
+                color: LongColor.button_primary_color,
+                textColor: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 7, horizontal: 0)
+              ),
+            )
+          ),
+          Padding(padding: EdgeInsets.only(right: 12)),
         ]
       )
     );
-    
   }
-}
-
-class NeteaseSearchPage extends StatefulWidget {
-  final Animation<double> animation;
-
-  const NeteaseSearchPage({Key key, @required this.animation}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return _NeteaseSearchPageState();
-  }
-}
-
-class _NeteaseSearchPageState extends State<NeteaseSearchPage> {
-  final TextEditingController _queryTextController = TextEditingController();
-
-  final FocusNode _focusNode = FocusNode();
-
-  String get query => _queryTextController.text;
-
-  set query(String value) {
-    assert(value != null);
-    _queryTextController.text = value;
-  }
-
-
-  bool initialState = true;
-
-
-  @override
-  void initState() {
-    super.initState();
-    _queryTextController.addListener(_onQueryTextChanged);
-    widget.animation.addStatusListener(_onAnimationStatusChanged);
-    _focusNode.addListener(_onFocusChanged);
-  }
-
-  @override
-  void dispose() {
-    _queryTextController.removeListener(_onQueryTextChanged);
-    widget.animation.removeStatusListener(_onAnimationStatusChanged);
-    _focusNode.removeListener(_onFocusChanged);
-    super.dispose();
-  }
-
-  ChatInput _searchHistory = ChatInput();
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-
-    Widget tabs;
-
-    return ScopedModel<ChatInput>(
-      model: _searchHistory,
-      child: Stack(
-        children: <Widget>[
-          DefaultTabController(
-            length: 2,
-            child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: theme.primaryColor,
-                iconTheme: theme.primaryIconTheme,
-                textTheme: theme.primaryTextTheme,
-                brightness: theme.primaryColorBrightness,
-                leading: BackButton(),
-                title: TextField(
-                  controller: _queryTextController,
-                  focusNode: _focusNode,
-                  style: theme.primaryTextTheme.title,
-                  textInputAction: TextInputAction.search,
-                  onSubmitted: (String _) => _search(query),
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintStyle: theme.primaryTextTheme.title,
-                      hintText: MaterialLocalizations.of(context).searchFieldLabel),
-                ),
-                actions: buildActions(context),
-                bottom: tabs,
-              ),
-              resizeToAvoidBottomInset: false,
-              // body: _BoxWithBottomChatController(),
-            ),
-          ),
-          SafeArea(child: Padding(padding: EdgeInsets.only(top: kToolbarHeight), ))
-        ],
-      ),
-    );
-  }
-
-  ///start search for keyword
-  void _search(String query) {
-    if (query.isEmpty) {
-      return;
-    }
-    _searchHistory.insertSearchHistory(query);
-    _focusNode.unfocus();
-    setState(() {
-      initialState = false;
-      // _searchedQuery = query;
-      this.query = query;
-    });
-  }
-
-  void _onQueryTextChanged() {
-    setState(() {
-      // rebuild ourselves because query changed.
-    });
-  }
-
-  void _onAnimationStatusChanged(AnimationStatus status) {
-    if (status != AnimationStatus.completed) {
-      return;
-    }
-    widget.animation.removeStatusListener(_onAnimationStatusChanged);
-    //we need request focus on text field when first in
-    FocusScope.of(context).requestFocus(_focusNode);
-  }
-
-  void _onFocusChanged() {
-    setState(() {});
-  }
-
-  List<Widget> buildActions(BuildContext context) {
-    return <Widget>[
-      query.isEmpty
-          ? null
-          : IconButton(
-              tooltip: '清除',
-              icon: const Icon(Icons.clear),
-              onPressed: () {
-                query = '';
-              },
-            )
-    ]..removeWhere((v) => v == null);
-  }
-
-  // Widget buildSuggestions(BuildContext context) {
-  //   if (query.isEmpty || !isSoftKeyboardDisplay(MediaQuery.of(context)) || !_focusNode.hasFocus) {
-  //     return Container(height: 0, width: 0);
-  //   }
-  //   return SuggestionOverflow(
-  //     query: query,
-  //     onSuggestionSelected: (keyword) {
-  //       query = keyword;
-  //       _search(query);
-  //     },
-  //   );
-  // }
 }
